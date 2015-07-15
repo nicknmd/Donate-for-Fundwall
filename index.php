5b9d4ca0538e9b115438e3dc74d5e7cefab2d01b
Bert Timmermans <berttimmermans.com@gmail.com>
2015-07-14 21:18:33 -0400

    basic modal / form styling

--- index.php
+++ index.php
@@ -80,77 +80,92 @@ if ($_POST) {
   </script>
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
   <script type="text/javascript" src="script.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 </head>
 <body>
-  <div class="wrapper">
-    <h1>
-      FundWall
-    </h1>
-
-    <div class="messages">
-      <!-- Error messages go here go here -->
-    </div>
-
+  <div class="app">
     <form action="#" method="POST" class="donation-form">
-      <fieldset>
-        <div class="form-row form-first-name">
-          <label>Name</label>
-          <input type="text" name="first-name" class="first-name text">
+      <div class="modal">
+        
+        <header>
+          <div class="logo">
+            <img src="assets/logo.svg" />
+          </div>
+          <h2>Belcham</h2>
+          <h3>Help us raise money to support Belgium students in the USA.</h3>
+        </header>
+    
+        <div class="messages">
+          <!-- Error messages go here go here -->
         </div>
         
-        <div class="form-row form-email">
-          <label>Email</label>
-          <input type="text" name="email" class="email text">
-        </div>
-
-        <div class="form-row form-message">
-          <label><i>Optional Message</i></label>
-          <input type="text" row="3" name="message" class="message text">
-        </div>
-
-        <div class="form-row form-amount">
-          <label><input type="radio" name="amount" class="set-amount" value="25"> $25</label>
-          <label><input type="radio" name="amount" class="set-amount" value="50"> $50</label>
-          <label><input type="radio" name="amount" class="set-amount" value="100"> $100</label>
-          <label><input type="radio" name="amount" class="other-amount" value="0"> Other:</label> <input type="text" class="amount text" disabled>
-        </div>
-        <div class="form-row form-number">
-          <label>Card Number</label>
-          <input type="text" autocomplete="off" class="card-number text" value="4242424242424242">
-        </div>
-        <div class="form-row form-cvc">
-          <label>CVC</label>
-          <input type="text" autocomplete="off" class="card-cvc text" value="123">
-        </div>
-        <div class="form-row form-expiry">
-          <label>Expiration Date</label>
-          <select class="card-expiry-month text">
-            <option value="01" selected>January</option>
-            <option value="02">February</option>
-            <option value="03">March</option>
-            <option value="04">April</option>
-            <option value="05">May</option>
-            <option value="06">June</option>
-            <option value="07">July</option>
-            <option value="08">August</option>
-            <option value="09">September</option>
-            <option value="10">October</option>
-            <option value="11">November</option>
-            <option value="12">December</option>
-          </select>
-          <select class="card-expiry-year text">
-            <option value="2015" selected>2015</option>
-            <option value="2016">2016</option>
-            <option value="2017">2017</option>
-            <option value="2018">2018</option>
-            <option value="2019">2019</option>
-            <option value="2020">2020</option>
-          </select>
-        </div>
-        <div class="form-row form-submit">
-          <input type="submit" class="submit-button" value="Submit Donation">
-        </div>
-      </fieldset>
+        <section>
+          
+          <div class="form-row form-first-name">
+            <input type="text" name="first-name" class="first-name text" placeholder="Your name" value="Nick Van Wallegem">
+          </div>
+          
+          <div class="form-row form-email">
+            <input type="text" name="email" class="email text" placeholder="Your email">
+          </div>
+  
+          <div class="form-row form-message">
+            <input type="text" row="3" name="message" class="message text" placeholder="Optional Message">
+          </div>
+          
+        </section>
+        
+        <section>
+  
+          <div class="form-row form-amount">
+            <label><input type="radio" name="amount" class="set-amount" value="25"> $25</label>
+            <label><input type="radio" name="amount" class="set-amount" value="50"> $50</label>
+            <label><input type="radio" name="amount" class="set-amount" value="100"> $100</label>
+            <label><input type="radio" name="amount" class="other-amount" value="0"> Other:</label> <input type="text" class="amount text" disabled>
+          </div>
+          
+          <div class="form-row form-number">
+            <input type="text" autocomplete="off" class="card-number text" value="4242424242424242">
+          </div>
+          
+          <div class="form-row form-cvc">
+            <input type="text" autocomplete="off" class="card-cvc text" value="123">
+          </div>
+          
+          <div class="form-row form-expiry">
+            <select class="card-expiry-month text">
+              <option value="01" selected>January</option>
+              <option value="02">February</option>
+              <option value="03">March</option>
+              <option value="04">April</option>
+              <option value="05">May</option>
+              <option value="06">June</option>
+              <option value="07">July</option>
+              <option value="08">August</option>
+              <option value="09">September</option>
+              <option value="10">October</option>
+              <option value="11">November</option>
+              <option value="12">December</option>
+            </select>
+            <select class="card-expiry-year text">
+              <option value="2015" selected>2015</option>
+              <option value="2016">2016</option>
+              <option value="2017">2017</option>
+              <option value="2018">2018</option>
+              <option value="2019">2019</option>
+              <option value="2020">2020</option>
+            </select>
+          </div>
+        
+        </section>
+        
+        <footer>
+          <div class="form-row form-submit">
+            <input type="submit" class="submit-button" value="Submit Donation">
+          </div>
+        </footer>
+        
+      </div>
     </form>
   </div>
 
