f94994c5497a74a01c3da800a6abbd60869663f9
nicknmd <nickvw@Nicks-MacBook-Pro.local>
2015-07-14 23:26:43 -0400

    Viewport settings thank you pages

--- done.php
+++ done.php
@@ -7,6 +7,8 @@
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 	<title>Stripe Donation Form</title>
   <link rel="stylesheet" type="text/css" href="style.css" media="all">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 </head>
 <body>
--- thankyou.php
+++ thankyou.php
@@ -42,6 +42,7 @@
 		   
 		   });  
   </script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 </head>
 <body>
   <div class="app">
